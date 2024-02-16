3f11f6c4a0f4860571743fa45bc1e969099aca98
RanjithKumarT35 <51657707+RanjithKumarT35@users.noreply.github.com>
2024-02-16 09:34:49 +0530

    Update Racing.js

--- src/forms/Racing.js
+++ src/forms/Racing.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState , useEffect } from "react";
 import axios from "axios";
 import url from "./global";
 function Racing() {
@@ -13,6 +13,9 @@ function Racing() {
     exprerience: "Select From DropDown",
     cc: "Select From DropDown",
   });
+  useEffect(async() => {
+    await axios.get(`${url}/`);
+  }, []);
   const handleSubmit = async (e) => {
     e.preventDefault();
 
