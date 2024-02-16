a5c427a8c86189a26a118a30ab3b5b2ba02e66a8
RanjithKumarT35 <51657707+RanjithKumarT35@users.noreply.github.com>
2024-02-16 11:01:08 +0530

    Update BikeConsultation.js

--- src/forms/BikeConsultation.js
+++ src/forms/BikeConsultation.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState ,useEffect } from "react";
 import axios from "axios";
 import url from "./global";
 function BikeConsultation() {
@@ -14,6 +14,9 @@ function BikeConsultation() {
     question: "",
     time: "Select From DropDown",
   });
+  useEffect(async() => {
+    await axios.get(`${url}/`);
+  }, []);
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
